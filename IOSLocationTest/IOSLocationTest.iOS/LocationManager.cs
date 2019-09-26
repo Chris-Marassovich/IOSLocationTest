261cc7b9a3d5649def1c4fea2efb792ef656271e
Chris <chris.marassovich@distillerysoftware.com>
2019-09-26 11:18:18 +1000

    Initial commit.

---
+++ IOSLocationTest/IOSLocationTest.iOS/LocationManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using CoreLocation;
+using UIKit;
+
+namespace IOSLocationTest.iOS
+{
+    public class LocationManager
+    {
+        private static readonly Lazy<LocationManager> instance = new Lazy<LocationManager>(() => new LocationManager());
+        public static LocationManager Instance { get { return instance.Value; } }
+
+        protected CLLocationManager locMgr;
+
+        public LocationManager()
+        {
+            this.locMgr = new CLLocationManager();
+            this.locMgr.PausesLocationUpdatesAutomatically = true;// = false;
+
+            // iOS 8 has additional permissions requirements
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                locMgr.RequestAlwaysAuthorization(); // works in background
+                //locMgr.RequestWhenInUseAuthorization (); // only in foreground
+            }
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
+            {
+                locMgr.AllowsBackgroundLocationUpdates = false;
+            }
+        }
+
+        public CLLocationManager LocMgr
+        {
+            get { return this.locMgr; }
+        }
+
+        public void StartLocationUpdates()
+        {
+            if (CLLocationManager.LocationServicesEnabled)
+            {
+                //set the desired accuracy, in meters
+                LocMgr.DesiredAccuracy = 1;
+                LocMgr.LocationsUpdated += (object sender, CLLocationsUpdatedEventArgs e) =>
+                {
+                    System.Diagnostics.Debug.WriteLine($"@@@@@@@@@@@@@@@@@LocationsUpdated {DateTime.Now} and count is {e.Locations.Count()}");
+                };
+                LocMgr.AllowsBackgroundLocationUpdates = true;
+                LocMgr.PausesLocationUpdatesAutomatically = false;
+                LocMgr.StartUpdatingLocation();
+            }
+        }
+    }
+}
---
+++ IOSLocationTest/IOSLocationTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IOSLocationTest.Models;
+
+namespace IOSLocationTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
